12b0df88df2a125583e36a7fc53ceb2723df3945
srmocher <sridhar687@gmail.com>
2017-02-18 16:32:14 -0700

    Code to handle events for rotations/movement

--- assignment2/viewer.cpp
+++ assignment2/viewer.cpp
@@ -30,6 +30,10 @@ vector<objInfo> objectInfos;
 vector<Object> sceneObjects;
 mat4 viewingMatrix;
 Scene scene;
+vec3 initialEye, initialCenter, initialViewUp;
+bool wireframe = false;
+
+void setLightProperties(objInfo info, mat4 mvMatrix);
 //----------------------------------------------------------------------------
 //
 // init
@@ -57,6 +61,9 @@ void init(void) {
 	//mat4 proj = frustum(bounds[0], bounds[1], bounds[2], bounds[3], bounds[4], bounds[5]);
 	SceneParser parser;
 	scene = parser.parseSceneFile("teapotScene.txt");
+	initialEye = scene.eye;
+	initialCenter = scene.center;
+	initialViewUp = scene.viewUp;
 	cout << "Center "<<scene.center.x<<","<<scene.center.y<<","<<scene.center.z<<endl;
 	cout << "Eye " << scene.eye.x << "," << scene.eye.y << "," << scene.eye.z << endl;
 	cout << "Num light sources :" << scene.numLightSources << endl;
@@ -68,23 +75,12 @@ void init(void) {
 	viewingMatrix = glm::lookAt(eye, center, viewUp);
 	ShaderInfo shaders[] = { {GL_VERTEX_SHADER,"BlinnPhong.vert"},
 	{GL_FRAGMENT_SHADER,"BlinnPhong.frag"},{GL_NONE,NULL} };
-	glEnable(GL_DEPTH_TEST);
+	
 	program = LoadShaders(shaders);
 	lightProperties props[4];
 	int numLightSources = scene.numLightSources;
 	
 	
-	for (int i = 1;i < numLightSources;i++)
-	{
-		lightProperties temp = scene.lightVals[i];
-		
-		vec4 tempVec(temp.position, 1.0);
-		vec4 t = viewingMatrix*tempVec;
-		t = t / t.w;
-		//scene.lightVals[i].position = vec3(t.x, t.y, t.z);
-
-		
-	}
 	for (auto i = 0;i < sceneObjects.size();i++)
 	{
 		int num;
@@ -120,21 +116,81 @@ void printMatrix(mat4 matrix)
 
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
 	float black[] = { 0.227, 0.474, 0.501,0.0 };
 	glClearBufferfv(GL_COLOR, 0, black);
 	glDepthMask(GL_TRUE);
 	glUseProgram(program);
-	mat4 tempMatrix = objInfos[0].modelMatrix;
-	mat4 projectionMatrix = glm::frustum(-1.0f, 1.f, -1.f, 1.0f, 1.5f, 3200.0f);
-	for (int i = 0;i < 2;i++)
+	
+	mat4 projectionMatrix = glm::frustum(-1.0f, 1.f, -1.f, 1.0f, 1.5f, 3200.0f);	
+	for (int i = 0;i < scene.numLightSources;i++)
+	{
+		lightProperties temp = scene.lightVals[i];
+
+		vec4 tempVec(temp.position, 1.0);
+		vec4 t = viewingMatrix*tempVec;
+		t = t / t.w;
+
+		scene.lightVals[i].position = vec3(t.x, t.y, t.z);
+	}
+	for (auto i = 0;i < objectInfos.size();i++)
+	{
+		objInfo info = objectInfos[i];
+		
+		mat4 modelingMatrix = info.modelMatrix;
+		mat4 mvMatrix = viewingMatrix*modelingMatrix;
+		glBindVertexArray(info.VAO);
+		setLightProperties(info, mvMatrix);
+		//printMatrix(mvMatrix);
+		
+		GLuint loc1 = glGetUniformLocation(program, "MVMatrix");	
+		glUniformMatrix4fv(loc1, 1, false, glm::value_ptr(mvMatrix));	
+
+		GLuint location = glGetUniformLocation(program, "NormalMatrix");
+		mat3 normalMatrix = mat3(mvMatrix);
+		glUniformMatrix3fv(location, 1, false, glm::value_ptr(normalMatrix));
+		
+		loc1 = glGetUniformLocation(program, "ambient");
+		glUniform3fv(loc1, 1, glm::value_ptr(info.Ka));
+
+		loc1 = glGetUniformLocation(program, "diffuse");
+		glUniform3fv(loc1, 1, glm::value_ptr(info.Kd));
+
+		loc1 = glGetUniformLocation(program, "specular");
+		glUniform3fv(loc1, 1, glm::value_ptr(info.Ks));
+
+		loc1 = glGetUniformLocation(program, "shininess");
+		glUniform1f(loc1, info.n);		
+		
+		//printMatrix(projectionMatrix);
+		mat4 mvpMatrix = projectionMatrix*viewingMatrix*modelingMatrix;
+		
+		loc1 = glGetUniformLocation(program, "MVPMatrix");
+		glUniformMatrix4fv(loc1, 1, false, glm::value_ptr(mvpMatrix));
+		
+		glDrawArrays(GL_TRIANGLES, 0, info.VAOsize);
+
+		GLenum error = glGetError();
+		if (error != GL_NO_ERROR) {
+			std::cerr << error << std::endl;
+			break;
+		}
+
+	}
+}
+
+void setLightProperties(objInfo info,mat4 mvMatrix)
+{
+	mat3 normalMatrix = glm::transpose(glm::inverse(mat3(mvMatrix)));
+	for (int i = 0;i < scene.numLightSources;i++)
 	{
 		lightProperties props = scene.lightVals[i];
 		char buff[100];
 		props.isEnabled = 1;
-		sprintf(buff, "Lights[%1d].isEnabled", i);
+		sprintf(buff, "Lights[%1d].isEnabled", i);		
 		GLuint loc = glGetUniformLocation(program, buff);
-		glUniform1i(loc, props.isEnabled);
-		
+		glUniform1i(loc, props.isEnabled);		
+
 		sprintf(buff, "Lights[%1d].isLocal", i);
 		loc = glGetUniformLocation(program, buff);
 		glUniform1i(loc, props.isLocal);
@@ -162,6 +218,7 @@ void display(void) {
 
 		sprintf(buff, "Lights[%1d].coneDirection", i);
 		loc = glGetUniformLocation(program, buff);
+		props.coneDirection = vec3(mvMatrix * vec4(props.coneDirection, 0.0));
 		glUniform3fv(loc, 1, glm::value_ptr(props.coneDirection));
 
 		sprintf(buff, "Lights[%1d].spotCosCutoff", i);
@@ -178,74 +235,102 @@ void display(void) {
 
 		sprintf(buff, "Lights[%1d].linearAttenuation", i);
 		loc = glGetUniformLocation(program, buff);
-		glUniform1f(loc, 0);
+		glUniform1f(loc, props.linearAttenuation);
 
 		sprintf(buff, "Lights[%1d].quadraticAttenuation", i);
 		loc = glGetUniformLocation(program, buff);
-		glUniform1f(loc, 0);
-	}
-	for (auto i = 0;i < objectInfos.size();i++)
-	{
-		objInfo info = objectInfos[i];
-		mat4 modelingMatrix = info.modelMatrix;
-		mat4 mvMatrix = viewingMatrix*modelingMatrix;
-		//printMatrix(mvMatrix);
-		GLuint loc1 = glGetUniformLocation(program, "MVMatrix");
-		mat3 normalMatrix = mat3(mvMatrix);
-		
-		glUniformMatrix4fv(loc1, 1, false, glm::value_ptr(mvMatrix));
-		loc1 = glGetUniformLocation(program, "NormalMatrix");
-		glUniformMatrix3fv(loc1, 1, false, glm::value_ptr(normalMatrix));
-		
-		loc1 = glGetUniformLocation(program, "ambient");
-		glUniform3fv(loc1, 1, glm::value_ptr(info.Ka));
-
-		loc1 = glGetUniformLocation(program, "diffuse");
-		glUniform3fv(loc1, 1, glm::value_ptr(info.Kd));
-
-		loc1 = glGetUniformLocation(program, "specular");
-		glUniform3fv(loc1, 1, glm::value_ptr(info.Ks));
-
-		loc1 = glGetUniformLocation(program, "shininess");
-		glUniform1f(loc1, info.n);
-
-		float *bounds = info.bounds;
-		vec4 left(bounds[0], bounds[2], bounds[4], 1.0f);
-		left = viewingMatrix*info.modelMatrix*left;
-		left = left / left.w;
-		vec4 right(bounds[1], bounds[3], bounds[5], 1.0f);
-		right = viewingMatrix*info.modelMatrix*right;
-		right = right / right.w;
-		float l = left.x;
-		float b = left.y;
-		float n = -left.z;
-
-		float top = right.y;
-		float r = right.x;
-		float f = -right.z;
-		//mat4 projectionMatrix = glm::frustum(0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f);
-		float zNear = (800 / 2.0f) / tan(45.0f*3.14159f / 360.0);
-		float zFar = 100000.0f;
-		
-		
-		//printMatrix(projectionMatrix);
-		mat4 mvpMatrix = projectionMatrix*viewingMatrix*info.modelMatrix;
-		
-		loc1 = glGetUniformLocation(program, "MVPMatrix");
-		glUniformMatrix4fv(loc1, 1, false, glm::value_ptr(mvpMatrix));
-		glBindVertexArray(info.VAO);
-		glDrawArrays(GL_TRIANGLES, 0, info.VAOsize);
-
-		GLenum error = glGetError();
-		if (error != GL_NO_ERROR) {
-			std::cerr << error << std::endl;
-			break;
-		}
-
+		glUniform1f(loc, props.quadraticAttenuation);
 	}
 }
 
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_W || key == GLFW_KEY_UP && action == GLFW_PRESS)
+	{
+		vec3 gazeVector = scene.center - scene.eye;
+		scene.eye = scene.eye + 0.1f * gazeVector;
+		scene.center = scene.center + 0.1f*gazeVector;
+		viewingMatrix = glm::lookAt(scene.eye, scene.center, scene.viewUp);
 
+	}
+	if (key == GLFW_KEY_A && action == GLFW_PRESS)
+	{
+		vec3 gaze = -(scene.center - scene.eye);
+		vec3 u = glm::cross(scene.viewUp, gaze);
+		mat4 tm = glm::translate(mat4(1.0f), u);
+		scene.center = scene.center + 0.2f*u;
+		scene.eye = scene.eye + 0.2f*u;
+		viewingMatrix = glm::lookAt(scene.eye, scene.center, scene.viewUp);
+	}
+	if (key == GLFW_KEY_S || key == GLFW_KEY_UP && action == GLFW_PRESS)
+	{
+		vec3 gazeVector = scene.center - scene.eye;
+		scene.eye = scene.eye - 0.1f * gazeVector;
+		scene.center = scene.center - 0.1f*gazeVector;
+		viewingMatrix = glm::lookAt(scene.eye, scene.center, scene.viewUp);
+	}
+	if (key == GLFW_KEY_D && action == GLFW_PRESS)
+	{
+		vec3 gaze = -(scene.center - scene.eye);
+		vec3 u = glm::cross(scene.viewUp, gaze);
+		scene.center = scene.center - 0.2f*u;
+		
+		scene.eye = scene.eye - 0.2f*u;
+		viewingMatrix = glm::lookAt(scene.eye, scene.center, scene.viewUp);
+	}
+	if (key == GLFW_KEY_P && action == GLFW_PRESS)
+	{
+		scene.eye = initialEye;
+		scene.center = initialCenter;
+		scene.viewUp = initialViewUp;
+		viewingMatrix = glm::lookAt(scene.eye, scene.center, scene.viewUp);
+	}
+	if (key == GLFW_KEY_O && action == GLFW_PRESS)
+	{
+		if (!wireframe) {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			wireframe = true;
+		}
+		else {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			wireframe = false;
+		}
+	}
+	if (key == GLFW_KEY_Q && action == GLFW_PRESS)
+	{
+		exit(1);
+	}
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		exit(1);
+	}
+	if (key == GLFW_KEY_X && action == GLFW_PRESS)
+	{
+		mat4 rm = glm::rotate(mat4(1.0), -1.0f*3.14159f / 180.0f, scene.viewUp);
+		scene.center = vec3(rm*vec4(scene.center, 1.0f));
+		scene.eye = vec3(rm*vec4(scene.eye, 1.0f));
+		viewingMatrix = glm::lookAt(scene.eye, scene.center, scene.viewUp);
+	}
+	if (key == GLFW_KEY_Z && action == GLFW_PRESS)
+	{
+		mat4 rm = glm::rotate(mat4(1.0), 1.0f*3.14159f / 180.0f, scene.viewUp);
+		scene.center = vec3(rm*vec4(scene.center, 1.0f));
+		scene.eye = vec3(rm*vec4(scene.eye, 1.0f));
+		viewingMatrix = glm::lookAt(scene.eye, scene.center, scene.viewUp);
+	}
+	if (key == GLFW_KEY_C && action == GLFW_PRESS)
+	{
+		mat4 rm = glm::rotate(mat4(1.0), 1.0f*3.14159f / 180.0f, scene.center - scene.eye);
+		scene.viewUp = vec3(rm*vec4(scene.viewUp,1.0f));
+		viewingMatrix = glm::lookAt(scene.eye, scene.center, scene.viewUp);
+	}
+	if (key == GLFW_KEY_V && action == GLFW_PRESS)
+	{
+		mat4 rm = glm::rotate(mat4(1.0), -1.0f*3.14159f / 180.0f, scene.center - scene.eye);
+		scene.viewUp = vec3(rm*vec4(scene.viewUp, 1.0f));
+		viewingMatrix = glm::lookAt(scene.eye, scene.center, scene.viewUp);
+	}
+}
 //----------------------------------------------------------------------------
 //
 // main
@@ -262,8 +347,8 @@ int main(int argc, char **argv)
   glfwInit();
 
 #ifdef __APPLE__
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); // 4.1 latest version of OpenGL for OSX 10.9 Mavericks
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // 4.1 latest version of OpenGL for OSX 10.9 Mavericks
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #endif
@@ -292,7 +377,11 @@ int main(int argc, char **argv)
   cout << "GLSL Version         :" << glslVersion << endl;
   cout << "major version: " << major << "  minor version: " << minor << endl;
 #endif
-
+  glfwSetKeyCallback(window, key_callback);
+ /* if (argc != 1)
+  {
+	  cout << "Enter fileName as parameter" << endl;
+  }*/
   init();
 
   while (!glfwWindowShouldClose(window)) {
